e08e12eda8abd5a406be51cad3c508a455b0ce38
marshmellow89 <pritamsingh100app@gmail.com>
2025-07-31 12:03:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 function createTable() {
     //Write your code here
+
+	let rn = prompt("Input number of rows");
+	rn = parseInt(rn);
+
+	let cn = prompt("Input number of columns");
+	cn = parseInt(cn);
+
+	let tr = document.createElement("tr");
+	let td = document.createElement("td");
+	const table = document.querySelector("#myTable");
+
+	if((!rn ||rn<=0)|| (!cn || cn<=0)){
+		alert("values are not defined");
+	}else{
+	for(let i=0;i<rn;i++){
+
+		for(let j=0;j<cn;j++){
+          td.textContent = `Row-${i} Column-${j}`
+			tr.appendChild(td);
+		}
+		table.appendChild(tr);
+	};
+	};
+
+	
   
 }
